e67b63711bcb33f77a6b8a576c7c1bb0aa4bd5e0
deluca42@gmail.com <*97178@git>
2022-09-28 21:02:38 -0300

    first commit

--- src/sorting/bubble/App.java
+++ src/sorting/bubble/App.java
@@ -1,6 +1,6 @@
 package sorting.bubble;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("bubble sort");
     }
 }
