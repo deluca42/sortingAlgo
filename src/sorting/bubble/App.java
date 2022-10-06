4136abe2966702d8637155f52990679d6ba552bb
deluca42@gmail.com <*97178@git>
2022-10-05 21:30:39 -0300

    impl of bubbleSort

--- src/sorting/bubble/App.java
+++ src/sorting/bubble/App.java
@@ -1,6 +1,58 @@
 package sorting.bubble;
+
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("bubble sort");
+        
+        //start at the beggining and compare first two elemets
+        // and switch in the case of bigger one. 
+        
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i =0;i < numbers.length; i++){
+            numbers[i] = rand.nextInt(100);
+        }
+        // antes da ordenacao / before sort 
+        printArray(numbers);
+        
+        bubbleSort(numbers);
+
+        printArray(numbers);
+        
+    }
+
+    private static void bubbleSort(int[] numbers) {
+        
+        Boolean keepSwap = true;
+        // switch numbers
+        while (keepSwap) {
+            keepSwap = false;
+            for (int i=0; i < numbers.length - 1 ; i ++) {
+
+                if (numbers[i]>numbers[i+1]) {
+                    //set keepswap true; 
+                    keepSwap = true;
+                    // keep biggest number
+                    int temp = numbers[i];
+                    // change smallest number to first position
+                    numbers[i] = numbers[i+1];
+                    // keep biggest into next position
+                    numbers[i+1] = temp;
+                }
+            }
+        }
+
+
+    }
+
+    private static void printArray(int[] numbers) {
+
+        for (int i : numbers) {
+            System.out.println(i);
+        }
+        System.out.println("ARRAY ###");
+
     }
 }
---
+++ src/sorting/insertion/insertionAlgo.java
@@ -0,0 +1,18 @@
+package sorting.insertion;
+
+
+public class insertionAlgo {
+    public static void main(String[] args) throws Exception {
+
+        int[] vetor = new int[10]; 
+
+        for (int i = 0; i < vetor.length; i ++){
+            System.out.println(i);
+        }
+
+        System.out.println("insertion sort antes : ");
+    }
+
+
+
+}
